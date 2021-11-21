aa2e8fca13b51d47e3793625a25c560222d983a9
Ellis John <ellisjohn@zipcodembpchuck.myfiosgateway.com>
2021-11-21 09:29:45 -0500

    lab

--- src/main/java/io/zipcoder/currencyconverterapplication/CurrencyType.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/CurrencyType.java
@@ -25,6 +25,64 @@ public enum CurrencyType {
     }
 
     public static CurrencyType getTypeOfCurrency(ConvertableCurrency currency) {
+
+
+        if (currency.getRate() == AUSTRALIAN_DOLLAR.rate) {
+            return AUSTRALIAN_DOLLAR;
+        }
+
+        if (currency.getRate() == CANADIAN_DOLLAR.rate) {
+            return CANADIAN_DOLLAR;
+        }
+
+        if (currency.getRate() == CHINESE_YR.rate) {
+            return CHINESE_YR;
+        }
+
+        if (currency.getRate() == EURO.rate) {
+            return EURO;
+        }
+
+        if (currency.getRate() == FRANC.rate) {
+            return FRANC;
+        }
+
+        if (currency.getRate() == POUND.rate) {
+            return POUND;
+        }
+
+        if (currency.getRate() == RINGGIT.rate) {
+            return RINGGIT;
+        }
+
+        if (currency.getRate() == RUPEE.rate) {
+            return RUPEE;
+        }
+
+        if (currency.getRate() == SINGAPORE_DOLLAR.rate) {
+            return SINGAPORE_DOLLAR;
+        }
+
+        if (currency.getRate() == UNIVERSAL_CURRENCY.rate) {
+            return UNIVERSAL_CURRENCY;
+        }
+
+        if (currency.getRate() == US_DOLLAR.rate) {
+            return US_DOLLAR;
+        }
+
+        if (currency.getRate() == YEN.rate) {
+            return YEN;
+        }
+
         return null;
+
+
+
+
+
+
+
+
     }
 }
